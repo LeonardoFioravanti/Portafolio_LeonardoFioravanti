eccca85a041a6e154b3715f977f7dfd16249195d
LeoFiora1 <156979915+LeoFiora1@users.noreply.github.com>
2024-12-01 19:46:53 -0600

    Semana 11
    Cambios semana 11

--- TiendaLeonardoFioravanti/src/main/java/service/CategoriaServiceimpl/CategoriaServiceImpl.java
+++ TiendaLeonardoFioravanti/src/main/java/service/CategoriaServiceimpl/CategoriaServiceImpl.java
@@ -1,30 +1,45 @@
 package service.CategoriaServiceimpl;
 
-
-import Tienda.demo.dao.CategoriaDAO;
-import Tienda.demo.domain.Categoria;
-
-
-
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import service.CategoriaService;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
+    //La anotacion autowired crea un unico objeto sin hacer new.
     @Autowired
-    private CategoriaDAO categoriaDao;
+    private CategoriaDao categoriaDao;
 
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-        var lista=categoriaDao.findAll();
+        var lista = categoriaDao.findAll();
         if (activos) {
-           lista.removeIf(e -> !e.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
